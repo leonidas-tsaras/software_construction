7f33531c6e4f1967661545cca0a59af44c56068b
leonidas <leonidas.tsaras@gmail.com>
2015-11-09 12:56:52 +0200

    ver. 0.1

---
+++ source/6_2.java
@@ -0,0 +1,13 @@
+static int findFirst(int[] arr, int val) {
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == val) return i;
+    }
+    return arr.length;
+}
+
+static int findLast(int[] arr, int val) {
+    for (int i = arr.length -1 ; i >= 0; i--) {
+        if (arr[i] == val) return i;
+    }
+    return -1;
+}
